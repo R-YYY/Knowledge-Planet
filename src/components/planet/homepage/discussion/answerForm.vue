40b65bd62a501072514fe633fe7834e9f7f1040b
leomessi7 <1367027689@qq.com>
2022-04-22 00:31:57 +0800

    讨论区小细节

---
+++ src/components/planet/homepage/discussion/answerForm.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="form">
+  <div>
+    <textarea v-model="content" @input="autoTextAreaHeight" class="input"></textarea>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "answerForm",
+  data(){
+    return{
+      content:""
+    }
+  },
+  methods:{
+    autoTextAreaHeight(e){
+      e.target.style.height= 'auto'
+      e.target.style.height = e.target.scrollHeight + "px"
+    }
+  }
+}
+</script>
+
+<style scoped>
+#form{
+  width: 90%;
+  margin: 20px 5%;
+  padding-bottom: 10px;
+  min-height: 250px;
+  border-radius: 12px;
+  background-color: #E2F5F0;
+}
+.input{
+  width: 92%;
+  font-size: 16px;
+  min-height: 150px;
+  line-height: 20px;
+  margin-left: 4%;
+  margin-top: 25px;
+  border-radius: 12px;
+  border: 0 solid #42b983;
+  box-sizing: border-box;
+  padding:10px 20px;
+  resize: none;
+}
+.input:focus{
+  outline-width: 0;
+}
+</style>
--- src/components/planet/homepage/discussion/discussCard.vue
+++ src/components/planet/homepage/discussion/discussCard.vue
@@ -2,7 +2,13 @@
   <div>
     <div class="desc">
       <h1 class="name">{{ topic.name }}</h1>
-      <p class="content">{{ topic.content }}</p>
+      <div class="text">
+        <span class="content" @click="contentVisible = !contentVisible">{{ contentVisible ? topic.content : topic.shortContent }}</span>
+        <span class="showButton" @click="contentVisible = !contentVisible">
+          {{ contentVisible ? '收起' : '显示全部' }}
+          <i :class="contentVisible?'el-icon-arrow-up':'el-icon-arrow-down'"></i>
+        </span>
+      </div>
     </div>
     <div class="opt">
       <img src="@/assets/icon/comment.png" alt="">
@@ -12,64 +18,110 @@
       <img src="@/assets/icon/thumb.png" alt="">
       <span>{{ topic.thumb_num > 99 ? "99+" : topic.thumb_num }}</span>
     </div>
-    <el-button style="position: absolute;right:50px" round icon="el-icon-edit" @click="dialogFormVisible=true">回答
+    <el-button id="answerButton" round icon="el-icon-edit" @click="answerFormVisible=true">回答
     </el-button>
+    <answerForm v-if="answerFormVisible"></answerForm>
     <hr class="line">
   </div>
 </template>
 
 <script>
+import answerForm from "@/components/planet/homepage/discussion/answerForm";
 export default {
   name: "discussCard",
-  props:['topic'],
+  props: ['topic'],
+  components:{
+    answerForm
+  },
+  data() {
+    return {
+      contentVisible: false,
+      answerFormVisible:true
+    }
+  },
+  created() {
+    let content = this.topic.content
+    this.topic.shortContent = ''
+    console.log(content.length)
+    let reg = /[^\x00-\xff]/
+    console.log(reg.test('一'))
+    let l = 0
+    if (content.length <= 70) {
+      this.topic.shortContent = content
+    } else {
+      for (let i = 0; i < content.length; i++) {
+        this.topic.shortContent += content.charAt(i)
+        l = (l + (reg.test(content.charAt(i)) ? 2 : 1))
+        if (l >= 160) {
+          break
+        }
+      }
+    }
+    this.topic.shortContent += '...'
+    console.log(this.topic.shortContent)
+    console.log(this.topic.shortContent.length)
 
+  }
 }
 </script>
 
 <style scoped>
 
 .desc {
+  position: relative;
   cursor: pointer;
+  padding: 0 20px;
 }
 
 .name {
-  margin: 20px 30px;
+  margin: 20px 0;
   color: #6A6868;
   font-weight: bold;
   font-size: 18px;
 }
 
+.text {
+  position: relative;
+  margin-bottom: 10px;
+}
+
 .content {
+  position: relative;
   color: #6A6868;
-  font-size: 15px;
+  font-size: 16px;
   line-height: 25px;
-  margin-left: 30px;
-  margin-right: 40px;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  line-clamp: 2;
-  -webkit-line-clamp: 2;
-  overflow: hidden;
-  text-overflow: ellipsis;
+  /*display: -webkit-box;*/
+  /*-webkit-box-orient: vertical;*/
+  /*line-clamp: 2;*/
+  /*-webkit-line-clamp: 2;*/
+  /*overflow: hidden;*/
+  /*text-overflow: ellipsis;*/
+}
+
+.showButton {
+  margin-left: 10px;
+  color: #74D8BE;
 }
 
 .line {
-  margin: 15px 30px 0px 30px;
-  border-top: 2px dashed #C1C0C0;
+  margin: 15px 20px 0px 20px;
+  border-top: 1px dashed #C1C0C0;
   border-bottom: 0;
 }
-.opt{
+
+.opt {
   width: 60px;
-  text-align: center;
+  text-align: left;
 }
+
 .opt img {
   width: 25px;
-  vertical-align: middle;
+  vertical-align: bottom;
 }
 
 .opt span {
   color: #999898;
-  font-size: 14px;
+  font-size: 16px;
   font-weight: bold;
   margin-left: 2px;
 }
@@ -77,11 +129,11 @@ export default {
 .opt {
   display: inline-block;
   cursor: pointer;
-  margin-left: 30px;
+  margin-left: 20px;
 }
 
 .el-button {
-  padding:  0 10px!important;
+  padding: 0 10px !important;
   height: 32px;
   line-height: 28px;
   font-weight: bold;
@@ -104,7 +156,7 @@ export default {
   padding-right: 7px;
 }
 
-#button {
-  margin-right: 10px;
+#answerButton {
+  position: absolute;right:50px
 }
 </style>
