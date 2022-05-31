a7d8a6616b7dd8175e49bda6fd5c3c5191d1c3cf
leomessi7 <1367027689@qq.com>
2022-05-31 21:00:54 +0800

    优化了发送请求时的界面闪白情况

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,35 @@
 import Vue from 'vue'
 import Axios from 'axios'
 import {Loading} from 'element-ui'
+
 const axiosInstance = Axios.create({
     baseURL: "http://49.235.232.7:81/kp",
     timeout: 5000
 })
 let loadingInstance = null
+let timer = null
 axiosInstance.interceptors.request.use(
     config => {
-        config.headers['Content-Type']= 'application/json;charset=UTF-8'
+        config.headers['Content-Type'] = 'application/json;charset=UTF-8'
         if (window.sessionStorage.getItem('token')) {
             config.headers['token'] = window.sessionStorage.getItem('token')
         }
-        loadingInstance = Loading.service({fullscreen:true})
+        if (!timer)
+            timer = setTimeout(() => {
+                loadingInstance = Loading.service({fullscreen: true})
+            }, 1000)
         return config
     },
     err => {
         Vue.prototype.$message.error('请求超时')
         return Promise.reject(err)
     }
-
 )
 
 axiosInstance.interceptors.response.use(
     response => {
-        loadingInstance.close()
+        if (timer) clearTimeout(timer)
+        if (loadingInstance) loadingInstance.close()
         return response
     },
     error => {
